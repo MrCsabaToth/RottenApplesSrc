eae63d8b3ceccceb2bd6c5fbcf5303f0fabbd870
MrCsabaToth <csaba.toth.us@gmail.com>
2015-06-06 16:44:21 -0700

    Placing pins

--- js/main.js
+++ js/main.js
@@ -3,16 +3,20 @@
         var self = this;
         self.initialized = false;
         self.is_spanish = false;
+        self.pins = [ { "lat":  36.740769, "lon": -119.798396, "name": "Tokyo Garden", "marker": null },
+                      { "lat":  36.743409, "lon": -119.798075, "name": "Fajita Fiesta", "marker": null },
+                      { "lat":  36.741715, "lon": -119.796637, "name": "Toledo's Mexican", "marker": null } ];
+        self.map = null;
 
         self.handleNoGeolocation = function(errorFlag) {
             if (errorFlag) {
-                var content = 'Error: The Geolocation service failed.';
+                var content = viewModel.is_spanish ? "Error: Ubicaci&oacute;n encontrado" : "Error: El servicio de Geolocalizaci&oacute;n fracas&oacute;.";
             } else {
-                var content = 'Error: Your browser doesn\'t support geolocation.';
+                var content = viewModel.is_spanish ? "Ubicaci&oacute;n encontrado" : "Error: Su navegador no soporta geolocalizaci&oacute;n.";
             }
 
             var options = {
-                map: map,
+                map: self.map,
                 position: new google.maps.LatLng(60, 105),
                 content: content
             };
@@ -24,7 +28,21 @@
 
         self.initialize = function() {
             $("#languageDialog").modal("show");
-        }
+        };
+
+        self.placeMarker = function(pindata) {
+            var latlon = new google.maps.LatLng(pindata.lat, pindata.lon);
+            var marker = new google.maps.Marker({
+                position: latlon,
+                icon: 'img/apple-map-pin.png',
+                map: self.map,
+            });
+            //var infowindow = new google.maps.InfoWindow({
+            //    content: pindata.name +
+            //        'Latitude: ' + location.lat() +
+            //        '<br>Longitude: ' + location.lng()
+            //});
+        };
     };
 
     $(document).ready(function() {
@@ -37,12 +55,13 @@
             $("#languageDialog").modal("hide");
         });
 
-        self.initializeMaps = function() {
+        window.initializeMaps = function() {
             var mapOptions = {
                 zoom: 12,
                 center: new google.maps.LatLng(36.742130, -119.795411)
             };
             var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+            viewModel.map = map;
 
             // Try HTML5 geolocation
             if(navigator.geolocation) {
@@ -51,20 +70,25 @@
                                                      position.coords.longitude);
 
                     var infowindow = new google.maps.InfoWindow({
-                        map: map,
+                        map: viewModel.map,
                         position: pos,
-                        content: 'Location found using HTML5.'
+                        content: viewModel.is_spanish ? "Ubicaci&oacute;n encontrado" : "Location found using HTML5."
                     });
 
                     map.setCenter(pos);
                     map.setZoom(18);
                     Pace.stop();
+
+                    // Place pins
+                    for(var i = 0; i < viewModel.pins.length; i++) {
+                        viewModel.placeMarker(viewModel.pins[i]);
+                    }
                 }, function() {
-                    self.handleNoGeolocation(true);
+                    viewModel.handleNoGeolocation(true);
                 });
             } else {
                 // Browser doesn't support Geolocation
-                self.handleNoGeolocation(false);
+                viewModel.handleNoGeolocation(false);
             }
         };
 
