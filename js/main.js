5078aa222e7ed17e4f065bf53a9970ab873c0cef
MrCsabaToth <csaba.toth.us@gmail.com>
2015-06-06 16:00:19 -0700

    Adding language selection dialog and geolocation comes only after that

--- js/main.js
+++ js/main.js
@@ -1,54 +1,86 @@
-$(document).ready(function() {
-    Pace.start();
+(function (window) {
+    var ViewModel = function () {
+        var self = this;
+        self.initialized = false;
+        self.is_spanish = false;
 
-    function handleNoGeolocation(errorFlag) {
-        if (errorFlag) {
-            var content = 'Error: The Geolocation service failed.';
-        } else {
-            var content = 'Error: Your browser doesn\'t support geolocation.';
+        self.handleNoGeolocation = function(errorFlag) {
+            if (errorFlag) {
+                var content = 'Error: The Geolocation service failed.';
+            } else {
+                var content = 'Error: Your browser doesn\'t support geolocation.';
+            }
+
+            var options = {
+                map: map,
+                position: new google.maps.LatLng(60, 105),
+                content: content
+            };
+
+            var infowindow = new google.maps.InfoWindow(options);
+            map.setCenter(options.position);
+            Pace.stop();
+        };
+
+        self.initialize = function() {
+            $("#languageDialog").modal("show");
         }
+    };
 
-        var options = {
-            map: map,
-            position: new google.maps.LatLng(60, 105),
-            content: content
-        };
+    $(document).ready(function() {
+        Pace.start();
+        var viewModel = new ViewModel();
+        viewModel.initialize();
 
-        var infowindow = new google.maps.InfoWindow(options);
-        map.setCenter(options.position);
-        Pace.stop();
-    }
+        $("#spanishbtn").click(function() {
+            viewModel.is_spanish = true;
+            $("#languageDialog").modal("hide");
+        });
 
-    function initialize() {
-        var mapOptions = {
-            zoom: 12,
-            center: new google.maps.LatLng(36.742130, -119.795411)
-        };
-        var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+        self.initializeMaps = function() {
+            var mapOptions = {
+                zoom: 12,
+                center: new google.maps.LatLng(36.742130, -119.795411)
+            };
+            var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-        // Try HTML5 geolocation
-        if(navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function(position) {
-                var pos = new google.maps.LatLng(position.coords.latitude,
-                                                 position.coords.longitude);
+            // Try HTML5 geolocation
+            if(navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(function(position) {
+                    var pos = new google.maps.LatLng(position.coords.latitude,
+                                                     position.coords.longitude);
 
-                var infowindow = new google.maps.InfoWindow({
-                    map: map,
-                    position: pos,
-                    content: 'Location found using HTML5.'
+                    var infowindow = new google.maps.InfoWindow({
+                        map: map,
+                        position: pos,
+                        content: 'Location found using HTML5.'
+                    });
+
+                    map.setCenter(pos);
+                    map.setZoom(18);
+                    Pace.stop();
+                }, function() {
+                    self.handleNoGeolocation(true);
                 });
+            } else {
+                // Browser doesn't support Geolocation
+                self.handleNoGeolocation(false);
+            }
+        };
 
-                map.setCenter(pos);
-                map.setZoom(18);
-                Pace.stop();
-            }, function() {
-                handleNoGeolocation(true);
-            });
-        } else {
-            // Browser doesn't support Geolocation
-            handleNoGeolocation(false);
-        }
-    }
+        $('#languageDialog').on('hidden.bs.modal', function () {
+            var script  = document.createElement('script');
+            script.type = "text/javascript";
+            scriptsrc = 'https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true&sensor=true';
+            if (viewModel.is_spanish)
+                scriptsrc += "&language=es";
+            scriptsrc += '&callback=initializeMaps';
+            console.log(scriptsrc);
+            script.src  = scriptsrc;
+            document.body.appendChild(script);
+            //document.getElementsByTagName('head')[0].appendChild(script);
+            //google.maps.event.addDomListener(window, 'load', viewModel.initializeMaps);
+        });
+    });
+} (window));
 
-    google.maps.event.addDomListener(window, 'load', initialize);
-});
